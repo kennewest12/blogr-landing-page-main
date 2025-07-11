6ce4a5ca9a66f83776fe0f45c475b9fb6b937f2d
Kenneth Chibuzor Okonkwo <kennethokonkwo73@gmail.com>
2025-07-11 09:17:05 -0700

    Move index.html to root, update style.css to use :root color variables

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 function toggleMenu() {
-  const navbar = document.querySelector('.navbar');
-  const menuOpen = document.getElementById('menu-open');
-  const menuClose = document.getElementById('menu-close');
+  const navbar = document.querySelector(".navbar");
+  const menuOpen = document.getElementById("menu-open");
+  const menuClose = document.getElementById("menu-close");
 
-  if (navbar.style.display === 'block') {
-    navbar.style.display = 'none';
-    menuOpen.style.display = 'block';
-    menuClose.style.display = 'none';
+  if (navbar.style.display === "block") {
+    navbar.style.display = "none";
+    menuOpen.style.display = "block";
+    menuClose.style.display = "none";
   } else {
-    navbar.style.display = 'block';
-    menuOpen.style.display = 'none';
-    menuClose.style.display = 'block';
+    navbar.style.display = "block";
+    menuOpen.style.display = "none";
+    menuClose.style.display = "block";
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Dropdown functionality
-  document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
-    toggle.addEventListener('click', (e) => {
+  document.querySelectorAll(".dropdown-toggle").forEach((toggle) => {
+    toggle.addEventListener("click", (e) => {
       e.preventDefault();
       let dropdownMenu = toggle.nextElementSibling;
 
-      if (dropdownMenu.style.display === 'block') {
-        dropdownMenu.style.display = 'none';
+      if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
       } else {
-        document.querySelectorAll('.dropdown').forEach(menu => {
-          menu.style.display = 'none'; // Close other dropdowns
+        document.querySelectorAll(".dropdown").forEach((menu) => {
+          menu.style.display = "none"; // Close other dropdowns
         });
-        dropdownMenu.style.display = 'block';
+        dropdownMenu.style.display = "block";
       }
     });
   });
